35aa905b771da835c8c81f6bb270d47c35980806
RohanBhatnagar <rohan.bha09@gmail.com>
2023-07-31 03:29:09 -0400

    multipg indiana works

--- selenium/indiana_scrape.py
+++ selenium/indiana_scrape.py
@@ -2,66 +2,66 @@ import csv
 from bs4 import BeautifulSoup
 import re
 
+
 def write(data):
-	soup = BeautifulSoup(data, 'html.parser')
+    soup = BeautifulSoup(data, 'html.parser')
 
-	shitfix = []
-	tables = soup.select(
-		'table[border="0"][cellpadding="0"][cellspacing="2"][width="680"]')
-	shitfix.append(tables)
+    shitfix = []
+    tables = soup.select(
+        'table[border="0"][cellpadding="0"][cellspacing="2"][width="680"]')
+    shitfix.append(tables)
 
-	names = []
-	addresses = []
-	city_state_zips = []
-	provider_types = []
-	specialties = []
-	pd_systems = []
-	phones = []
-	counties = []
+    names = []
+    addresses = []
+    city_state_zips = []
+    provider_types = []
+    specialties = []
+    pd_systems = []
+    phones = []
+    counties = []
 
+    def parse_string(data):
+        for i in range(0, len(data), 1):
+            line = data[i]
+            if line == "Address:":
+                names.append(data[i - 1])
+                addresses.append(data[i+1])
+            elif line == "City, State, Zip:":
+                city_state_zips.append(data[i+1])
+            elif line == "Provider Type:":
+                provider_types.append(data[i+1])
+            elif line == "Specialty:":
+                specialties.append(data[i+1])
+            elif line == "Program or Delivery System:":
+                pd_systems.append(data[i+1])
+            elif line == "Phone:":
+                phones.append(data[i+1])
+            elif line == "County:":
+                counties.append(data[i+1])
 
-	def parse_string(data):
-		for i in range(0, len(data), 1):
-			line = data[i]
-			if line == "Address:":
-				names.append(data[i - 1])
-				addresses.append(data[i+1])
-			elif line == "City, State, Zip:":
-				city_state_zips.append(data[i+1])
-			elif line == "Provider Type:":
-				provider_types.append(data[i+1])
-			elif line == "Specialty:":
-				specialties.append(data[i+1])
-			elif line == "Program or Delivery System:":
-				pd_systems.append(data[i+1])
-			elif line == "Phone:":
-				phones.append(data[i+1])
-			elif line == "County:":
-				counties.append(data[i+1])
+    def sub_write():
+        str = ""
+        for table in shitfix:
+            spans = soup.select(
+                'span', style="font-size:small; font-family:Arial")
+            for i in range(0, len(spans), 1):
+                str += '\n'
+                str += remove_whitespace(spans[i].text)
 
-	def sub_write():
-		str = ""
-		for table in shitfix:
-			spans = soup.select('span', style="font-size:small; font-family:Arial")
-			for i in range(0, len(spans), 1):
-				str += '\n'
-				str += remove_whitespace(spans[i].text)
+            arr = str.splitlines()
+            parse_string(arr)
 
-			arr = str.splitlines()
-			parse_string(arr)
+        csv_arr = []
+        for i in range(0, len(names), 1):
+            csv_line = [names[i], addresses[i], city_state_zips[i], provider_types[i],
+                        specialties[i], pd_systems[i], phones[i], counties[i]]
+            csv_arr.append(csv_line)
 
-			with open('indiana_output.csv', 'w', newline='') as csvfile:
-				for i in range(0, len(names), 1):
-					csv_line = [names[i], addresses[i], city_state_zips[i], provider_types[i],
-								specialties[i], pd_systems[i], phones[i], counties[i]]
-					csv_writer = csv.writer(csvfile)
-					csv_writer.writerow(csv_line)
+        return csv_arr
 
+    def remove_whitespace(text):
+        text = text.strip()
+        return re.sub(r'\s{2,}', ' ', text)
 
-	def remove_whitespace(text):
-		text = text.strip()
-		return re.sub(r'\s{2,}', ' ', text)
-
-
-	sub_write()
-	
+    return_arr = sub_write()
+    return return_arr
